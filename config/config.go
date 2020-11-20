b71671947353c7fbfe4384ca8339a2e2623463d4
Asa6 <18518353480@163.com>
2020-11-20 14:05:00 +0800

    连接mysql数据库，并区分不同数据库环境的配置文件

---
+++ config/config.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"runtime"
+)
+
+// 数据库信息
+type DBInfo struct {
+	Datatype string `yaml:"datatype"`
+	Hostname string `yaml:"hostname"`
+	Database string `yaml:"database"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Port     string `yaml:"port"`
+}
+
+// 区分数据库环境
+type Database struct {
+	Debug   DBInfo
+	Test    DBInfo
+	Release DBInfo
+}
+
+//获取数据库配置
+func (d *Database) GetInfo() DBInfo {
+	// 获取操作系统类型
+	ostype := runtime.GOOS
+
+	// 获取当前目录的路径
+	dir, _ := os.Getwd()
+
+	// 定义filePath变量，存储数据库配置文件路径
+	var filePath string
+
+	//兼容Linux和windows的文件路径
+	if ostype == "windows" {
+		filePath = dir + "\\config\\database.yaml"
+	} else if ostype == "linux" {
+		filePath = dir + "/config/database.yaml"
+	}
+
+	// 读取数据库配置文件
+	if fileData, err := ioutil.ReadFile(filePath); err != nil {
+		fmt.Println(err)
+	} else {
+		//解析yaml文件内容
+		err := yaml.Unmarshal(fileData, &d)
+		if err != nil {
+			log.Fatalf("cannot unmarshal data: %v", err)
+		}
+
+		// 返回当前环境的数据库信息
+		if gin.Mode() == gin.DebugMode {
+			return d.Debug
+		} else if gin.Mode() == gin.TestMode {
+			return d.Test
+		} else {
+			return d.Release
+		}
+	}
+
+	// 默认返回debug数据库信息
+	return d.Debug
+}
+
+func (d *DBInfo) GetConnect() {
+	// 数据库连接信息格式化
+	connArgs := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", d.Username, d.Password, d.Hostname, d.Port, d.Database)
+
+	// 连接数据库
+	db, err := gorm.Open(d.Datatype, connArgs)
+	if err != nil {
+		log.Fatalf("数据库连接失败：%v", err)
+	}
+
+	// 全局禁用表复数
+	db.SingularTable(true)
+	defer db.Close()
+}
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-//import "github.com/gin-gonic/gin"
-
-import . "GinDemo/router"
+import (
+	. "GinDemo/config"
+	. "GinDemo/router"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// 设置gind模式
+	gin.SetMode(gin.DebugMode)
+
+	var d Database
+	dbInfo := d.GetInfo()
+	dbInfo.GetConnect()
+	println(dbInfo.Password)
+	println(dbInfo.Username)
+	println(dbInfo.Database)
+
 	// 路由挂载
 	r := InitRouter()
 	r.Run(":8999") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
